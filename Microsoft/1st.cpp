af3957df1bf36a46665e691c9cda87c1ee3e7b05
Sumit Maity <89305385+Sumitmaity45@users.noreply.github.com>
2023-01-02 21:01:19 +0530

    Update 1st.cpp

--- Microsoft/1st.cpp
+++ Microsoft/1st.cpp
@@ -1,1 +1,44 @@
-
+# Evaluate Reverse Polish Notation
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>ans;
+        for(string x:tokens)
+        {
+            if(x=="+")
+            {
+                int a=ans.top();
+                ans.pop();
+                int b=ans.top();
+                ans.pop();
+                ans.push(a+b);
+            }
+            else if(x=="*")
+            {
+                int a=ans.top();
+                ans.pop();
+                int b=ans.top();
+                ans.pop();
+                ans.push(a*b);
+            }
+            else if(x=="-")
+            {
+                int a=ans.top();
+                ans.pop();
+                int b=ans.top();
+                ans.pop();
+                ans.push(b-a);
+            }
+            else if(x=="/")
+            {
+                int a=ans.top();
+                ans.pop();
+                int b=ans.top();
+                ans.pop();
+                ans.push(b/a);
+            }
+            else ans.push(stoi(x));
+        }
+        return ans.top();
+    }
+};
